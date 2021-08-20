2cb17dfc5eaeb194d2377b5c2b00d4dd272816c3
Ricky Alvarez <rawalvarez731@gmail.com>
2021-08-19 23:57:10 -0500

    fixed back button on posts

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -15,7 +15,11 @@ const Posts : NextPage = () => {
                     <li><h1>Digital Hell, Personal Blog</h1></li>
                     <li>Scroll down to see more</li>
                     <li ><h1 style={{fontSize: 9}}><span className='italic'>Demo site built by Raul Duke</span></h1></li>
-                    <Link href="/" passHref><li>Back</li></Link>
+                    <Link href="/" passHref><motion.li 
+                        initial={{backgroundColor: 'white', color: 'black'}}
+                        whileTap={{opacity: 1, backgroundColor: 'black', color: 'white'}}
+                        transition={{duration: 0.3}}
+                        whileHover={{opacity: 1, backgroundColor: 'black', color: 'white'}}className='button'>Back</motion.li></Link>
                 </ul>
             </div >
             <motion.div
