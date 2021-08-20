f4dd7a1ee598d782eb549f87c951d39607659ac0
Ricky Alvarez <rawalvarez731@gmail.com>
2021-08-19 23:02:31 -0500

    fixed layout wrapper tsx error

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,14 +4,9 @@ import Footer from './footer'
 const Layout : React.FC = (prop) => {
     return (
         <>
-            <div className='container'>
-                {/* TODO: Add simple nav bar */}
-                <div className='main'>
-                    { prop.children }
-                </div>
-                <Footer>
-                </Footer>
-            </div>
+            { prop.children }
+            <Footer>
+            </Footer>
         </>
     )
 };
