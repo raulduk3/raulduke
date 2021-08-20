b14127dae888a97a3e4d64ce3963c5ffe53c2f21
Ricky Alvarez <rawalvarez731@gmail.com>
2021-08-19 23:33:41 -0500

    added favicons to footer and made them link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 import style from '../styles/Footer.module.css'
+import { FaInstagram, FaYoutube, FaInfo} from "react-icons/fa";
 
 const Footer : React.FC = ({ children }) => {
     return (
         <div className={style.footer}>
             <div className={style.row}>
-                <div>
-                    {/* TODO: ADD FOOTER ELEMENTS */}
+                <div className={style.row}>
+                    <Link href="https://www.instagram.com/raulduk3" passHref><FaInstagram></FaInstagram></Link>
+                    <Link href="https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ" passHref><FaYoutube></FaYoutube></Link>
+                    <Link href="https://link.raulduke.com" passHref><FaInfo></FaInfo></Link>
                 </div>
             </div>
             <div className={style.row}>
