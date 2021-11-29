f5f237e8229824c4bfd91d347368a61564fa261d
Ricky Alvarez <rawalvarez731@gmail.com>
2021-11-29 18:11:15 -0500

    new font thx eli

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,10 @@ const Index: NextPage = () => {
 				<motion.h1
 					initial={{opacity: 0, y: -60}}
 					animate={{ opacity: [0, 1], y: [-60, 0] }}
-					style={{ fontSize: "4rem" }}
+					style={{ fontSize: "4.5rem", textAlign:"center", padding: "0.8em 0.5em", margin: "0" }}
 					transition={{ duration: 2, type: "tween"}}>RAUL DUKE</motion.h1>
 				<ul>
-					<li><a href="https://link.raulduke.com">LINKS</a></li>
+					<li><a href="https://link.raulduke.com">VIDEOS & MORE</a></li>
 				</ul>
 				<div className="news">
 					<p>RAUL DUKE IS TEMPORARILY OUT OF OPERATION.</p>
