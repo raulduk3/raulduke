ef2eb319a8cb415ee50b06693e6acce218a19cde
Ricky Alvarez <rawalvarez731@gmail.com>
2022-01-05 13:22:26 -0600

    very nice

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,16 +6,14 @@ import { FaInstagram, FaYoutube, FaInfo} from "react-icons/fa";
 const Footer : React.FC = ({ children }) => {
     return (
         <div className={style.footer}>
-            <div className={style.row}>
-                <div className={style.row}>
-                    <Link href="https://www.instagram.com/rauldukevfx/" passHref><FaInstagram></FaInstagram></Link>
-                    <Link href="https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ" passHref><FaYoutube></FaYoutube></Link>
-                    <Link href="https://link.raulduke.com" passHref><FaInfo></FaInfo></Link>
-                </div>
+            <div className={style.rowText}>
+                <h6 className={style.footerText}>© Copyright 2022</h6>
             </div>
-            <div className={style.row}>
-                <h6>© Copyright 2022</h6>
-            </div>
+            {/* <div className={style.rowList}>
+                <Link href="https://link.raulduke.com" passHref><FaInfo></FaInfo></Link>
+                <Link href="https://www.instagram.com/rauldukevfx/" passHref><FaInstagram></FaInstagram></Link>
+                <Link href="https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ" passHref><FaYoutube></FaYoutube></Link>
+            </div> */}
         </div>
     )
 };
