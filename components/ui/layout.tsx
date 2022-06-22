149b4ba07fc8eb19c3a5f5bf4589796091e658db
Richard Alvarez <richardalvarez@Richards-MacBook-Pro.local>
2022-06-22 18:57:12 -0500

    shit @

--- components/ui/layout.tsx
+++ components/ui/layout.tsx
@@ -8,15 +8,15 @@ const Layout : React.FC = ({ children }) => {
     return (
         <>
             <Head>
-                <title>website of zǝɹɐʌןɐ ʞɔıɹ</title>
+                <title>website of ɹᴉɔʞ ɐlʌɐɹǝz</title>
                 <meta property="og:type" content="website" />
                 <meta property="og:image" content="https://raulduke.com/images/BANNER.jpg" />
-                <meta property="og:title" content="Rick Noisy" key="title" />
-                <meta property="og:site_name" content="Rick Noisy"/>
+                <meta property="og:title" content="Rick Alvarez" key="title" />
+                <meta property="og:site_name" content="Rick Alvarez"/>
                 <meta property="og:url" content="https://raulduke.com"></meta>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta property="og:description" name="description" content="Multimedia | VFX | Video Editing & Production"/>
-                <meta name="keywords" content="Rick Noisy, rick, noisy, dvi, canon xl2, Canon XL2, glitch art, raul duke, Raoul Duke, Raul Duke, underground artist, vfx, chicago, music scene, music videos, art, poetry, latinx art, video art, code, programming, p5js, 5ouley, Undercurrent, chicago underground artists, cgi, animation, punk art, you tube music videos, youtube music videos, nudity in music videos, chicago underground film festival"/>
+                <meta name="keywords" content="RickAlvarez, rick,Alvarez, dvi, canon xl2, Canon XL2, glitch art, raul duke, Raoul Duke, Raul Duke, underground artist, vfx, chicago, music scene, music videos, art, poetry, latinx art, video art, code, programming, p5js, 5ouley, Undercurrent, chicago underground artists, cgi, animation, punk art, you tube music videos, youtube music videos, nudity in music videos, chicago underground film festival"/>
                 <meta name="copyright" content="Richard Alvarez" />
                 <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
                 <meta name="language" content="English" />
