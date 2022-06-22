725fdd7550fec694afec875247bb0325112dec3f
Richard Alvarez <richardalvarez@Richards-MacBook-Pro.local>
2022-06-22 07:03:37 -0500

    shit @

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ const Index = () => {
 
   return (
     <section className={style.root}>
-      <a href='https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ'>rick álvarez</a>
+      <p><a href='https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ'>rick álvarez</a></p>
+      <p>🝰🜛🜂</p>
       <ReactPlayer width="100vw" height="33vh" url='https://youtu.be/SXnGUEzP0p8' />
       <ReactPlayer width="100vw" height="33vh" url='https://youtu.be/CO70NVlY0Gw' />
       <ReactPlayer width="100vw" height="33vh" url='https://youtu.be/IWqAFtIGrUI' />
