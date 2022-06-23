59614f763a06084f01ff91f2677dac1feefa6c65
Richard Alvarez <richardalvarez@richards-mbp.lan>
2022-06-23 04:27:28 -0500

    yes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,9 @@ const Index = () => {
 
   return (
     <section className={style.root}>
-      <div className='header'>
-        <h6>🝰🜂🝲</h6>
-        <p className='main'><a href='https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ'>rick álvarez</a></p>
-        <h6 className='sub'><span className='flip'>Filmmaker</span>, <span className='flip'>Programmer</span>, <span className='flip'>Net Artist</span>,  <span className='flip'>Lover</span></h6>
-      </div>
+       <h6>🝰🜂🝲</h6>
+      <p><a href='https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ' className='main'>rick álvarez</a></p>
       <div className={style.column}>
-        <div className='label'>
-            <h3>○ Archive ○</h3>
-        </div>  
         <ReactPlayer  url='https://youtu.be/SXnGUEzP0p8' />
         <ReactPlayer url='https://youtu.be/IWqAFtIGrUI' />
         <ReactPlayer url='https://youtu.be/CO70NVlY0Gw' />
