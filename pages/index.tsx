73d55ec4ce41487ec8c9bd0069635b5e0ab4a194
Richard Alvarez <richardalvarez@richards-mbp.lan>
2022-06-23 04:19:54 -0500

    shit @

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
       <div className='header'>
         <h6>🝰🜂🝲</h6>
         <p className='main'><a href='https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ'>rick álvarez</a></p>
-        <h6 className='sub'><span className='flip'>Filmmaker</span> / <span className='flip'>Programmer</span> / <span className='flip'>Net Artist</span> / <span className='flip'>Lover</span></h6>
+        <h6 className='sub'><span className='flip'>Filmmaker</span>, <span className='flip'>Programmer</span>, <span className='flip'>Net Artist</span>,  <span className='flip'>Lover</span></h6>
       </div>
       <div className={style.column}>
         {/* <div className='label'>
