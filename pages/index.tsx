a75b93844a51b0f62c1cfbf7a51aa68c680a0d49
Richard Alvarez <richardalvarez@Richards-MacBook-Pro.local>
2022-08-08 16:37:51 -0400

    new vids

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ const Index = () => {
   return (
     <section className={style.root}>
       <div className="header">
-        <h6>🝰🜂🝲</h6>
+        <h6>🝰🜂🜛</h6>
         <p><a href='https://www.youtube.com/channel/UCtn5rjH9-l1LeHULyc8udwQ' className='main'>rick álvarez</a></p>
-        </div>
+      </div>
       <div className={style.column}>  
-        <ReactPlayer  url='https://youtu.be/SXnGUEzP0p8' />
-        <ReactPlayer url='https://youtu.be/IWqAFtIGrUI' />
+        <ReactPlayer  url='https://youtu.be/SDDqeX3K6xc' />
+        <ReactPlayer url='https://youtu.be/rSVpTN6pRjA' />
         <ReactPlayer url='https://youtu.be/CO70NVlY0Gw' />
         <ReactPlayer url='https://youtu.be/z_O_n9QNYKo' />
         <ReactPlayer url='https://youtu.be/OiXkIOF98A0' />
