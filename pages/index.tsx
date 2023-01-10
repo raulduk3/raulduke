71a65792aed3d75f1c6580c5e98508d26d4ce94a
richard álvarez <rawalvarez731@gmail.com>
2023-01-10 15:37:06 -0600

    fixed merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import { useMediaQuery } from 'react-responsive'
@@ -127,29 +126,3 @@ const Index = () => {
 }
 
 export default Index
-=======
-import React from 'react';
-import ReactPlayer from 'react-player'
-import style from '../styles/Index.module.css'
-
-const Index = () => {
-
-  return (
-    <section className={style.root}>
-      <div className="header">
-        <h6>🝰🜂🜛</h6>
-        <p><a href='https://vimeo.com/rickyalvarez' className='main'>rick álvarez</a></p>
-      </div>
-      <div className={style.column}>  
-        <ReactPlayer url='https://youtu.be/6rMKwRTi2cs' />
-        <ReactPlayer url='https://youtu.be/rSVpTN6pRjA' />
-        <ReactPlayer url='https://youtu.be/CO70NVlY0Gw' />
-        <ReactPlayer url='https://youtu.be/z_O_n9QNYKo' />
-        <ReactPlayer url='https://youtu.be/OiXkIOF98A0' />
-      </div>
-    </section>
-  );
-};
-
-export default Index;
->>>>>>> master
