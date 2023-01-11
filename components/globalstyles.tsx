5cef8f7b6ed2ca1c21146043bd674a572d590d23
richard álvarez <rawalvarez731@gmail.com>
2023-01-10 19:42:43 -0600

    charlie fixed

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -8,7 +8,8 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
+    overflow: hidden;
+    }
   button {
         color: black;
         border: none;
