37eab7bacf3639fdc4358e0cd28741d220636632
richard álvarez <rawalvarez731@gmail.com>
2023-01-11 02:04:44 -0600

    finished links

--- styles/index.js
+++ styles/index.js
@@ -125,7 +125,7 @@ export const Menu = styled.div.attrs(props => ({
     align-items: center;
     justify-content: center;
     width: 100vw;
-    height: 100vh;
+    height: 100%;
     position: absolute;
     top: 0;
     background-color: ${({ theme }) => theme.colors.secondary};
