a80a9dab4f1fcc3567c6d49e7590f685975b966b
richard álvarez <rawalvarez731@gmail.com>
2023-01-12 00:07:32 -0600

    charlie fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -181,7 +181,7 @@ const Index = ({ router }) => {
                             ))}
                             </Grid>}
                         {currentPage == "#about" && <>
-                                <Image alt='Me-- very gooofy' style={{marginTop: '0.5em', marginBottom: '0.2em'}} height={140} src="/AnyConv.com__F6B71112-67B7-45E0-B7C2-1A7DDB7C4076_1_102_o.jpg" width={100}></Image>
+                                <Image alt='Me-- very gooofy' style={{marginTop: '0.5em', marginBottom: '1.2em'}} height={140} src="/AnyConv.com__F6B71112-67B7-45E0-B7C2-1A7DDB7C4076_1_102_o.jpg" width={100}></Image>
                                 <TextBlob>
                                  A collection of my work as a filmmaker and digital media artist. 
                                  <br></br><br></br>
@@ -202,7 +202,7 @@ const Index = ({ router }) => {
                                 <RA>(coming soon)</RA>
                             </>}
                     </Container>
-                    {(currentPage != '' && currentPage != '#') && <Link style={{ marginTop: '0.5em', marginBottom: '1.5em', fontSize: '0.45em' }} href="#" onClick={(e) => handleNavClick('')}><Underline>Back</Underline></Link> }
+                    {(currentPage != '' && currentPage != '#') && <Link style={{ margin: '2.5em', fontSize: '0.45em' }} href="#" onClick={(e) => handleNavClick('')}><Underline>Back</Underline></Link> }
                 </Container>
                 <Footer />
             </PageWrapper>
