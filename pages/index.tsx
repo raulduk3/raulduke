429cd5f73c8c98205b8c5a983bf8e16ddaf5defe
richard álvarez <rawalvarez731@gmail.com>
2023-01-12 12:28:18 -0600

    charlie fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -157,7 +157,7 @@ const Index = ({ router }) => {
                                 <HamburgerWrapper onClick={handleClick} />
                                 <MenuWrapper size={size} open={open} onClick={handleClick}>
                                     <Link href="#" onClick={(e) => handleNavClick('')}>Home</Link>
-                                    <Link href="#about" onClick={(e) => handleNavClick('#about')}>{currentPage != "#about" ? <i>About</i>: 'About'}</Link>
+                                    <Link href="#about" onClick={(e) => handleNavClick('#about')}>{currentPage == "#about" ? <i>About</i>: 'About'}</Link>
                                     <Link href="#projects" onClick={(e) => handleNavClick('#projects')}>{currentPage == "#projects" ? <i>Projects</i> : 'Projects'}</Link>
                                     <Link href="#archive" onClick={(e) => handleNavClick('#archive')}>{currentPage == "#archive" ? <i>Archive</i> : 'Archive'}</Link>
                                 </MenuWrapper>
