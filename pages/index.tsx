c7cb0c71a8fb572e9d3b88bd1059fa06cf942496
richard álvarez <rawalvarez731@gmail.com>
2023-01-12 12:02:57 -0600

    charlie fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -169,7 +169,7 @@ const Index = ({ router }) => {
                         <SocialLinks></SocialLinks>
                     </Hero>
                     <Container>
-                        {(currentPage.includes("#")) &&
+                        {!(currentPage.includes("#")) &&
                             <Grid>
                             {videos.map((video) => (
                                 <GridItem key={video.title}>
