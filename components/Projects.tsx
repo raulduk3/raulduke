d8badc226b97303b5716074c6e83486bec4bac9b
richard álvarez <rawalvarez731@gmail.com>
2023-01-11 21:04:00 -0600

    finished

---
+++ components/Projects.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { Link } from '../styles/index'
+
+const Projects = () => {
+  const [links, setLinks] = useState([
+      { name: 'Sentiment Analysis of Rotten Tomato Reviews', url: 'https://github.com/morty-c137-prime/sa-rotten-tamatoes' },
+      { name: 'raulduke.com', url: 'https://github.com/morty-c137-prime/raulduke' },
+
+  ]);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsVisible(true), 300);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div>
+      <div style={{
+        textAlign: 'center',
+        fontSize: '0.8em'
+      }}>
+        {links.map((link, index) => (
+          <h4
+            key={link.name}
+            style={{
+              opacity: isVisible ? 1 : 0,
+              transition: 'opacity 0.3s',
+              transitionDelay: `${index * 0.1}s`,
+            }}
+          >
+            <Link href={link.url}>{link.name}</Link>
+          </h4>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+  
+
+export default Projects;
---
+++ components/randomString.tsx
@@ -0,0 +1,54 @@
+import { Small } from '../styles/index'
+import React, { useState, useEffect } from "react";
+
+const characters = 
+"!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ⼀⼁⼂⼃⼄⼅⼆⼇⼈⼉⼊⼋⼌⼍⼎⼏⼐⼑⼒⼓⼔⼕⼖⼗⼘⼙⼚⼛⼜⼝⼞⼟⼠⼡⼢⼣⼤⼥⼦⼧⼨⼩⼪⼫⼬⼭⼮⼯⼰⼱⼲⼳⼴⼵⼶⼷⼸⼹⼺⼻⼼⼽⼾⼿⽀⽁⽂⽃⽄⽅⽆⽇⽈⽉⽊⽋⽌⽍⽎⽏⽐⽑⽒⽓⽔⽕⽖⽗⽘⽙⽚⽛⽜⽝⽞⽟⽠⽡⽢⽣⽤⽥⽦⽧⽨⽩⽪⽫⽬⽭⽮⽯⽰⽱⽲⽳⽴⽵⽶⽷⽸⽹⽺⽻⽼⽽⽾⽿";
+
+function RandomString() {
+    const [finalString, setFinalString] = useState("");
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(() => {
+      generateRandomString();
+  
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("touchstart", handleMouseMove);
+      window.addEventListener("scroll", handleMouseMove);
+
+  
+      return () => {
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("touchstart", handleMouseMove);
+        window.removeEventListener("scroll", handleMouseMove);
+      };
+    }, []);
+  
+    function generateRandomString() {
+      let random = "";
+      for (let i = 0; i < 1000; i++) {
+        random += characters[Math.floor(Math.random() * characters.length)];
+      }
+      const index = Math.floor(Math.random() * random.length);
+      const final = random.slice(0, index) + ' RICKY ÁLVAREZ ' + random.slice(index);
+      setFinalString(final);
+      setLoading(false);
+    }
+
+    function handleMouseMove() {
+      generateRandomString();
+    }
+  
+    if (loading) {
+      return <Small>Loading...</Small>
+    }
+  
+    return (
+      <Small style={{
+        letterSpacing: '0.5em'
+      }}>{finalString}</Small>
+    );
+  }
+  
+  export default RandomString;
+  
+  
