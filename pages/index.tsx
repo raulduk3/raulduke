29da72e2a4742ee42fef30c2c6b28b515197bf6d
richard álvarez <rawalvarez731@gmail.com>
2023-01-14 12:53:19 -0600

    charlie fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -178,9 +178,7 @@ function Index({ router }) {
                             {videos.map((video) => (
                                 <GridItem key={video.title}>
                                     <a href={video.url}>
-                                        <img src={video.image} style={{
-                                            maxHeight: '100%'
-                                        }} alt={video.title} />
+                                        <img src={video.image} width={300} alt={video.title} />
                                     </a>
                                     <h3>{video.title}</h3>
                                 </GridItem>
