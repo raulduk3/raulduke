7c96e0e1105b560f9ff29b7c4cfcf1625797ac01
ra - our lady <rawalvarez731@gmail.com>
2023-06-07 14:40:58 -0500

    Refactored

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,10 +3,12 @@ import { Link } from '../styles/index'
 
 export default function Projects() {
   const [links, setLinks] = useState([
+      { name: 'Captivating Video Editing Secrets: Unsupervised Deep Learning and PySceneDetect Analysis of Creative Strategies', url: 'https://digital.kenyon.edu/dh_iphs_ss/22/'},
       { name: 'Sentiment Analysis of Rotten Tomato Reviews', url: 'https://github.com/morty-c137-prime/sa-rotten-tamatoes' },
       { name: 'film-project-template', url: 'https://github.com/morty-c137-prime/film-project-template' },
       { name: 'chat-app', url: 'https://github.com/raulduk3/chat-app' },
       { name: 'raulduke.com', url: 'https://github.com/morty-c137-prime/raulduke' },
+      { name: 'Gund Gallery Immersive Experience', url: 'https://github.com/srabieh/GundGalleryGroup5' }
       
 
   ]);
