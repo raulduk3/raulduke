17d4702e80930b80ad7a2c0a099997d9cde8738b
ra - our lady <rawalvarez731@gmail.com>
2023-06-07 15:24:57 -0500

    Adjusted -- mostly done refactoring code for noew

--- styles/global.tsx
+++ styles/global.tsx
@@ -4,8 +4,8 @@ const GlobalStyles = createGlobalStyle`
     /* Add global styles here, such as reset styles, fonts, and colors */
 
     #__next {
-        height: 100%;
-        width: 100%;
+        height: 100vh;
+        width: 100vw;
         max-width: 100%;
     }
 
@@ -15,7 +15,7 @@ const GlobalStyles = createGlobalStyle`
         overflow-x: hidden;
         font-family: 'Open Sans', sans-serif;
         max-width: 100%;
-        height: 100%;
+        height: 100vh;
         color: ${({ theme }) => theme.colors.primary};
     }
 `
