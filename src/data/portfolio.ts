71f943518f7a6f87674fcb374053992b35c1ecdc
ra - our lady <rawalvarez731@gmail.com>
2025-07-29 22:48:02 -0500

    done

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -11,13 +11,13 @@ export const portfolio: PortfolioItem[] = [
     {
         title: "Joaquín Morales",
         url: "https://joaquinmoralesdp.com/",
-        description: "Portfolio for cinematographer",
+        description: "Portfolio",
         external: true,
     },
     {
         title: "Machine Television",
         url: "https://machinetelevision-git-main-richard-alvarezs-projects.vercel.app/",
-        description: "Digital market for skate brand",
+        description: "E-commerce",
         external: false,
     },
     // Add more here!
