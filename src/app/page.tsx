c098682d902f61238ed4e8192c4a3864d403c30c
ra - our lady <rawalvarez731@gmail.com>
2025-12-02 16:39:24 -0600

    bump

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,20 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-dvh w-full">
-      <section className="w-full max-w-xl mb-6">
+      <section className="w-full max-w-xl mb-6 flex flex-col items-center">
         <h1 className="text-3xl font-black tracking-wide uppercase text-center mb-1">Richard Álvarez</h1>
-        
+
         <p className="text-orange text-center text-lg font-light mb-1 text-left tracking-normal">Web Developer & AI Engineer</p>
         <p className="max-w-md text-base text-white mb-2 text-left">
           I build creative, effective, and minimal internet experiences.<br />
-          <span className="text-white">Based in Chicago, working globally.</span>
-        </p>
-        <div className="flex md:flex-row flex-col md:gap-2 mt-2 mb-2 text-left">
+          <span className="text-white">Based in Chicago, working globally.</span><br />
           <a
             href="mailto:rawalvarez731@gmail.com"
             className="no-underline text-orange hover:text-white"
           >
-            email
+            → email
           </a>
-          <a
-            href="https://forms.gle/KnJiVfGeGaz8Ngd56"
-            className="no-underline text-orange hover:text-white"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            website request form
-          </a>
-        </div>
+        </p>
       </section>
 
       {/* <section className="w-full max-w-xl">
