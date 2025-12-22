592c356c049485ab90779d9d9c50e0c151447cea
ra - our lady <rawalvarez731@gmail.com>
2025-12-22 09:21:48 -0600

    new resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 // src/app/page.tsx
-import { portfolio } from "@/data/portfolio";
 import Link from "next/link";
 
 export default function HomePage() {
@@ -20,44 +19,6 @@ export default function HomePage() {
           </a>
         </p>
       </section>
-
-      {/* <section className="w-full max-w-xl">
-        <h2 className="text-xl font-semibold uppercase mb-2 tracking-wide text-left">Websites</h2>
-        <ul className="text-left pl-5 space-y-0.5 tracking-normal">
-          {portfolio.map((item) =>
-            item.external ? (
-              <li key={item.title} className="leading-tight flex items-center gap-1.5">
-                <span className="text-white text-sm leading-tight">▪</span>
-                <div className="flex items-center gap-2">
-                  <a
-                    href={item.url}
-                    className="text-orange underline hover:text-white whitespace-nowrap"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {item.title}
-                  </a>
-                  {item.description && (
-                    <span className="text-white text-sm whitespace-nowrap">{item.description}</span>
-                  )}
-                </div>
-              </li>
-            ) : (
-              <li key={item.title} className="leading-tight flex items-center gap-1.5">
-                <span className="text-white text-sm leading-tight">▪</span>
-                <div className="flex items-center gap-2">
-                  <Link href={item.url} className="text-orange no-underline hover:text-white whitespace-nowrap">
-                    {item.title}
-                  </Link>
-                  {item.description && (
-                    <span className="text-white text-sm whitespace-nowrap">{item.description}</span>
-                  )}
-                </div>
-              </li>
-            )
-          )}
-        </ul>
-      </section> */}
     </main>
   );
 }
